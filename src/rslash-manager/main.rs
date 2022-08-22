d313159fecc867a19c7087ebe090dfefac94d5d1
Murray Groves <github@murraygrov.es>
2022-08-22 12:41:48 +0100

    Kubernetes Done, Gfycat handling and Simple Get
    - Shards  should autoscale.
    - Downloader gets direct link to gfycat content
    - Can use /get (not complete)

--- src/rslash-manager/main.rs
+++ src/rslash-manager/main.rs
@@ -1,10 +1,11 @@
 use std::env;
-use std::process::Command;
+use std::process::Command as Cmd;
 use kube::api::PatchParams;
 use serde_json;
 use redis;
 use redis::Commands;
 use std::cmp;
+use std::collections::HashMap;
 use std::fs::read;
 use std::thread::current;
 use std::convert::TryInto;
@@ -12,6 +13,14 @@ use tokio::time::sleep;
 use std::time::Duration;
 use kube::api::Patch;
 use base64;
+use serenity::Client;
+use serenity::client::{Context, EventHandler};
+use serenity::prelude::{GatewayIntents, TypeMapKey};
+use serenity::async_trait;
+use serenity::builder::CreateApplicationCommandOption;
+use serenity::model::application::command::{Command, CommandOptionType};
+use serenity::model::gateway::Ready;
+use serenity::model::interactions::application_command::ApplicationCommand;
 
 /*let client_k8s = kube::Client::try_default().await.expect("Failed to connect to k8s");
 
@@ -23,7 +32,7 @@ let total_shards: serde_json::Value = serde_json::from_str(total_shards).unwrap(
 let total_shards: u64 = total_shards["spec"]["replicas"].as_u64().unwrap(); */
 
 async fn get_redis_connection() -> redis::Connection {
-    let mut get_ip = Command::new("kubectl");
+    let mut get_ip = Cmd::new("kubectl");
     get_ip.arg("get").arg("nodes").arg("-o").arg("json");
     let output = get_ip.output().expect("Failed to run kubectl");
     let ip_output = String::from_utf8(output.stdout).expect("Failed to convert output to string");
@@ -191,27 +200,144 @@ async fn start() {
     let _:() = con.set("manual_sharding", "false").expect("Failed to set manual_sharding"); // Tells discord-interface to take control of sharding again
 }
 
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    /// Fires when the client is connected to the gateway
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        println!("Updating slash commands");
+        let command = ctx.data.read().await;
+        let command = command.get::<command_to_update>().unwrap();
+
+        if command == "get" || command == "all" {
+            println!("Matched on get");
+            let subreddits = ["gifs", "pics"]; // TODO - Replace this with a call to the new DB
+
+            let _ = Command::create_global_application_command(&ctx.http, |command| {
+                command
+                    .name("get")
+                    .description("Get a post from a specified subreddit");
+
+                command.create_option(|option| {
+                    option.name("subreddit")
+                        .description("The subreddit to get a post from")
+                        .required(true)
+                        .kind(CommandOptionType::String);
+
+                    for subreddit in subreddits {
+                        option.add_string_choice(subreddit, subreddit);
+                    }
+
+                    return option;
+                });
+
+                return command;
+            }).await.expect("Failed to register slash commands");
+        }
+        println!("Exiting");
+        std::process::exit(0);
+    }
+}
+
+struct command_to_update;
+impl TypeMapKey for command_to_update {
+    type Value = String;
+}
+
+async fn update_commands(command: Option<&str>) {
+    let command: &str = match command {
+        Some(x) => x,
+        None => "all"
+    };
+
+    let token = env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN not set");
+    let application_id: u64 = env::var("DISCORD_APPLICATION_ID").expect("DISCORD_APPLICATION_ID not set").parse().expect("Failed to convert application_id to u64");
+    let shard_id: usize = 0;
+    let total_shards: usize = env::var("TOTAL_SHARDS").expect("TOTAL_SHARDS not set").parse().expect("Failed to convert total_shards to usize");
+
+    let mut client = Client::builder(token, GatewayIntents::non_privileged() | GatewayIntents::GUILD_MEMBERS)
+        .event_handler(Handler)
+        .application_id(application_id)
+        .await
+        .expect("Error creating client");
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<command_to_update>(command.to_string());
+    }
+
+    client.start_shard(shard_id as u64, total_shards as u64).await.expect("Error starting shard");
+
+}
+
 
 async fn update() {
-    update_max_unavailable().await;
+    let component = env::args_os().nth(2).unwrap().into_string().unwrap();
 
-    match {env::args_os().nth(2)} { // Check if a tag is provided
-        Some(x) => {
-            let tag: String = x.into_string().unwrap().parse().unwrap();
-            let mut rollout = Command::new("kubectl");
-            rollout.arg("set").arg("-n").arg("r-slash").arg("image").arg("statefulset/discord-shards").arg(format!("discord-shard=discord-shard:{}", tag));
-            let output = rollout.output().expect("Failed to run kubectl");
-            println!("{:?}", output.stdout);
-            println!("{:?}", output.stderr);
+    match component.as_str() {
+        "shards" => {
+            update_max_unavailable().await;
+
+            match {env::args_os().nth(3)} { // Check if a tag is provided
+                Some(x) => {
+                    let tag: String = x.into_string().unwrap().parse().unwrap();
+                    let mut rollout = Cmd::new("kubectl");
+                    rollout.arg("set").arg("-n").arg("r-slash").arg("image").arg("statefulset/discord-shards").arg(format!("discord-shard=discord-shard:{}", tag));
+                    let output = rollout.output().expect("Failed to run kubectl");
+                    println!("{:?}", String::from_utf8(output.stdout).unwrap());
+                    println!("{:?}", String::from_utf8(output.stderr).unwrap());
+                },
+                None => {
+                    let mut rollout = Cmd::new("kubectl");
+                    rollout.arg("rollout").arg("-n").arg("r-slash").arg("restart").arg("statefulset/discord-shards");
+                    let output = rollout.output().expect("Failed to run kubectl");
+                    println!("{:?}", String::from_utf8(output.stdout).unwrap());
+                    println!("{:?}", String::from_utf8(output.stderr).unwrap());
+                }
+            };
         },
-        None => {
-            let mut rollout = Command::new("kubectl");
-            rollout.arg("rollout").arg("-n").arg("r-slash").arg("restart").arg("statefulset/discord-shards");
-            let output = rollout.output().expect("Failed to run kubectl");
-            println!("{:?}", output.stdout);
-            println!("{:?}", output.stderr);
+
+        "subreddits" => {
+            let action = env::args_os().nth(3).unwrap().into_string().unwrap();
+            match action.as_str() {
+                "add" => {
+                    update_commands(Some("get")).await;
+                },
+                "remove" => {
+
+                },
+                _ => {
+                    println!("Invalid action");
+                    return;
+                }
+            }
+        },
+
+        "downloader" => {
+            match {env::args_os().nth(3)} { // Check if a tag is provided
+                Some(x) => {
+                    let tag: String = x.into_string().unwrap().parse().unwrap();
+                    let mut rollout = Cmd::new("kubectl");
+                    rollout.arg("set").arg("-n").arg("r-slash").arg("image").arg("deployment/reddit-downloader").arg(format!("reddit-downloader=reddit-downloader:{}", tag));
+                    let output = rollout.output().expect("Failed to run kubectl");
+                    println!("{:?}", String::from_utf8(output.stdout).unwrap());
+                    println!("{:?}", String::from_utf8(output.stderr).unwrap());
+                },
+                None => {
+                    let mut rollout = Cmd::new("kubectl");
+                    rollout.arg("rollout").arg("-n").arg("r-slash").arg("restart").arg("deployment/reddit-downloader");
+                    let output = rollout.output().expect("Failed to run kubectl");
+                    println!("{:?}", String::from_utf8(output.stdout).unwrap());
+                    println!("{:?}", String::from_utf8(output.stderr).unwrap());
+                }
+            };
         }
-    };
+        _ => {
+            println!("No component/invalid component provided");
+        }
+    }
+
 }
 
 
