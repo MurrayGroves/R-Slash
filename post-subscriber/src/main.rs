e409d72ca93a12b53450457591fda22c5fa742b3
Murray Groves <github@murraygrov.es>
2024-06-23 18:10:54 +0100

    Subscriber side of subscriptions

--- post-subscriber/src/main.rs
+++ post-subscriber/src/main.rs
@@ -1,4 +1,180 @@
+use futures::{Future, StreamExt};
+use mongodb::bson::doc;
+use mongodb::options::ClientOptions;
+use serde_derive::{Deserialize, Serialize};
+use serenity::all::{ChannelId, CreateMessage, GatewayIntents, Http};
 use serenity::{all::EventHandler, async_trait};
+
+use log::{debug, error, info, warn};
+use tarpc::server::incoming::Incoming;
+use tokio::sync::Mutex;
+use std::collections::HashSet;
+use std::net::SocketAddr;
+use std::env;
+use std::io::Write;
+use std::sync::Arc;
+use anyhow::anyhow;
+
+
+use futures::future::{self, Ready};
+use tarpc::{
+    client, context,
+    server::{self, Channel},
+    tokio_serde::formats::Bincode,
+};
+
+
+#[tarpc::service]
+trait Subscriber {
+    async fn register_subscription(subreddit: String, channel: u64) -> Result<(), String>;
+
+    async fn delete_subscription(subreddit: String, channel: u64) -> Result<(), String>;
+
+    async fn list_subscriptions(channel: u64) -> Result<Vec<Subscription>, String>;
+
+    async fn notify(subreddit: String, post_id: String) -> Result<(), String>;
+
+    async fn watched_subreddits() -> Result<HashSet<String>, String>;
+}
+
+#[derive(Clone)]
+struct SubscriberServer {
+    socket_addr: SocketAddr,
+    db: Arc<Mutex<mongodb::Client>>,
+    subscriptions: Arc<Mutex<Vec<Subscription>>>,
+    discord: Arc<Http>,
+    redis: redis::aio::MultiplexedConnection,
+}
+
+impl Subscriber for SubscriberServer {
+    async fn register_subscription(self, _: context::Context, subreddit: String, channel: u64) -> Result<(), String> {
+        info!("Registering subscription for subreddit {} to channel {}", subreddit, channel);
+
+        let client = self.db.lock().await;
+        let coll: mongodb::Collection<Subscription> = client.database("state").collection("subscriptions");
+
+        let subscription = Subscription {
+            subreddit: subreddit,
+            channel: channel
+        };
+
+        match coll.insert_one(&subscription, None).await {
+            Ok(_) => (),
+            Err(e) => return Err(e.to_string())
+        };
+
+        self.subscriptions.lock().await.push(subscription);
+        Ok(())
+    }
+
+    async fn delete_subscription(self, _: context::Context, subreddit: String, channel: u64) -> Result<(), String> {
+        info!("Deleting subscription for subreddit {} to channel {}", subreddit, channel);
+
+        let client = self.db.lock().await;
+        let coll: mongodb::Collection<Subscription> = client.database("state").collection("subscriptions");
+
+        let filter = doc! {
+            "subreddit": &subreddit,
+            "channel": channel as i64
+        };
+
+        match coll.delete_one(filter, None).await {
+            Ok(_) => (),
+            Err(e) => return Err(e.to_string())
+        
+        };
+        self.subscriptions.lock().await.retain(|sub| sub.subreddit != subreddit || sub.channel != channel);
+        Ok(())
+    }
+
+    // List subscriptions for a channel
+    async fn list_subscriptions(self, _: context::Context, channel: u64) -> Result<Vec<Subscription>, String> {
+        info!("Listing subscriptions");
+
+        let client = self.db.lock().await;
+        let coll: mongodb::Collection<Subscription> = client.database("state").collection("subscriptions");
+
+        let filter = doc!{
+            "channel": channel as i64
+        };
+
+        let mut cursor = match coll.find(filter, None).await {
+            Ok(cursor) => cursor,
+            Err(e) => return Err(e.to_string())
+        };
+        let mut subscriptions = Vec::new();
+
+        while let Some(sub) = cursor.next().await {
+            subscriptions.push(sub.unwrap());
+        }
+
+        Ok(subscriptions)
+    }
+
+    async fn notify(self, _: context::Context, subreddit: String, post_id: String) -> Result<(), String> {
+        info!("Notifying subscribers of post {} in subreddit {}", post_id, subreddit);
+
+        let subscriptions = self.subscriptions.lock().await;
+        let filtered = subscriptions.iter().filter(|sub| sub.subreddit == subreddit).collect::<Vec<&Subscription>>();
+
+        let mut redis = self.redis.clone();
+
+        let mut post = match post_api::get_post_by_id(&post_id, None, &mut redis, None).await {
+            Ok(post) => post,
+            Err(e) => return Err(e.to_string())
+        };
+
+        // Remove the components because we don't want autopost and refresh options in this context
+        post.components = None;
+
+        for sub in filtered {
+            info!("Notifying channel {} of post {} in subreddit {}", sub.channel, post_id, subreddit);
+
+            let post = post.clone();
+
+            let channel: ChannelId = sub.channel.into();
+            
+            let mut resp = CreateMessage::default();
+            if let Some(em) = post.embed {
+                resp = resp.embed(em);
+            }
+
+            if let Some(content) = post.content {
+                resp = resp.content(content);
+            }
+
+            if let Some(attachment) = post.file {
+                resp = resp.add_file(attachment);
+            }
+
+            match channel.send_message(&self.discord, resp).await {
+                Ok(_) => (),
+                Err(e) => return Err(e.to_string())
+            }
+        }
+
+        Ok(())
+    }
+
+    async fn watched_subreddits(self, _: context::Context) -> Result<HashSet<String>, String> {
+        let subscriptions = self.subscriptions.lock().await;
+        let subreddits: HashSet<String> = subscriptions.iter().map(|sub| sub.subreddit.clone()).collect();
+
+        info!("Listing watched subreddits {:?}", subreddits);
+
+        Ok(subreddits)
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Subscription {
+    subreddit: String,
+    channel: u64
+}
+
+async fn spawn(fut: impl Future<Output = ()> + Send + 'static) {
+    tokio::spawn(fut);
+}
 
 struct Handler;
 
@@ -22,10 +198,46 @@ async fn main() {
     let posthog_key: String = env::var("POSTHOG_API_KEY").expect("POSTHOG_API_KEY not set").parse().expect("Failed to convert POSTHOG_API_KEY to string");
     let posthog_client = posthog::Client::new(posthog_key, "https://eu.posthog.com/capture".to_string());
 
-    let mut client_options = ClientOptions::parse("mongodb+srv://my-user:rslash@mongodb-svc.r-slash.svc.cluster.local/admin?replicaSet=mongodb&ssl=false").await.unwrap();
+    let mut client_options = ClientOptions::parse("mongodb://r-slash:r-slash@mongodb-primary.discord-bot-shared.svc.cluster.local/admin?ssl=false").await.unwrap();
     client_options.app_name = Some("Post Subscriber".to_string());
-    let mongodb_client = mongodb::Client::with_options(client_options).unwrap();
+    let mongodb_client = Arc::new(Mutex::new(mongodb::Client::with_options(client_options).unwrap()));
+
+    let subscriptions = Arc::new(Mutex::new(Vec::new()));
+
+    let db_client = redis::Client::open("redis://redis.discord-bot-shared/").unwrap();
+    let redis = db_client.get_multiplexed_async_connection().await.expect("Can't connect to redis");
 
     let mut client =
-        serenity::Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+    serenity::Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+
+    let http = client.http.clone();
+
+    let mut listener = tarpc::serde_transport::tcp::listen("0.0.0.0:50051", Bincode::default).await.unwrap();
+    listener.config_mut().max_frame_length(usize::MAX);
+    tokio::spawn(listener
+        // Ignore accept errors.
+        .filter_map(|r| future::ready(r.ok()))
+        .map(server::BaseChannel::with_defaults)
+        // serve is generated by the service attribute. It takes as input any type implementing
+        // the generated SubscriberServer trait.
+        .map(move |channel| {
+            let server = SubscriberServer {
+                socket_addr: channel.transport().peer_addr().unwrap(),
+                db: Arc::clone(&mongodb_client),
+                subscriptions: Arc::clone(&subscriptions),
+                discord: Arc::clone(&http),
+                redis: redis.clone()
+            };
+            channel.execute(server.serve()).for_each(spawn)
+        })
+        // Max 10 channels.
+        .buffer_unordered(10)
+        .for_each(|_| async {})
+    );
+
+    info!("Started tarpc server, connecting to discord...");
+
+    if let Err(why) = client.start().await {
+        error!("Client error: {:?}", why);
+    }
 }
--- rslash_types/src/lib.rs
+++ rslash_types/src/lib.rs
@@ -1,31 +1,74 @@
-use std::{collections::HashMap, sync::RwLock};
+use std::time::Duration;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+
+#[derive(Debug, Clone)]
+pub struct PostRequest {
+    pub channel: ChannelId,
+    pub subreddit: String,
+    pub search: Option<String>,
+    // Interval between posts in seconds
+    pub interval: Duration,
+    pub limit: Option<u32>,
+    pub current: u32,
+    pub last_post: Instant,
+    pub author: UserId,
 }
 
-/// Represents a value stored in a [ConfigStruct](ConfigStruct)
-pub enum ConfigValue {
-    U64(u64),
-    RoleId(serenity::model::prelude::RoleId),
-    Bool(bool),
-    REDIS(redis::aio::Connection),
-    MONGODB(mongodb::Client),
-    SubredditList(Vec<String>),
-    PosthogClient(posthog::Client),
+pub enum AutoPostCommand {
+    Start(PostRequest),
+    Stop(ChannelId),
 }
+
+use serenity::all::{ChannelId, CreateActionRow, UserId};
+use tokio::time::Instant;
+
 
 /// Stores config values required for operation of the shard
+#[derive(Debug, Clone)]
 pub struct ConfigStruct {
-    pub shard_id: u64,
+    pub shard_id: u32,
     pub nsfw_subreddits: Vec<String>,
+    pub auto_post_chan: tokio::sync::mpsc::Sender<AutoPostCommand>,
+    pub redis: redis::aio::MultiplexedConnection,
 }
 
 impl serenity::prelude::TypeMapKey for ConfigStruct {
     type Value = ConfigStruct;
 }
+
+#[derive(Debug, Clone)]
+pub struct InteractionResponse {
+    pub file: Option<serenity::builder::CreateAttachment>,
+    pub embed: Option<serenity::all::CreateEmbed>,
+    pub content: Option<String>,
+    pub ephemeral: bool,
+    pub components: Option<Vec<CreateActionRow>>,
+    pub fallback: ResponseFallbackMethod,
+}
+
+impl Default for InteractionResponse {
+    fn default() -> Self {
+        InteractionResponse {
+            file: None,
+            embed: None,
+            content: None,
+            ephemeral: false,
+            components: None,
+            fallback: ResponseFallbackMethod::Error
+        }
+    }
+}
+
+// What to do if sending a response fails due to already being acknowledged
+#[derive(Debug, Clone)]
+pub enum ResponseFallbackMethod {
+    /// Edit the original response
+    Edit,
+    /// Send a followup response
+    Followup,
+    /// Return an error
+    Error,
+    /// Do nothing
+    None
+}
+
