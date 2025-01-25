1e0f35596edf69c20771fc2f9a8443bb62da3978
Murray Groves <github@murraygrov.es>
2025-01-25 17:26:01 +0000

    feat: can login to dashboard
    doesn't retrieve user information yet

--- website/src/lib/types.ts
+++ website/src/lib/types.ts
@@ -1,4 +1,13 @@
 export enum Bot {
-	BB = "Booty Bot",
-	RS = "R Slash"
+	BB = 'Booty Bot',
+	RS = 'R Slash'
+}
+
+export function botShorthand(bot: Bot): string {
+	switch (bot) {
+		case Bot.BB:
+			return 'bb';
+		case Bot.RS:
+			return 'rs';
+	}
 }
